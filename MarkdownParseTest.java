0fedc154988e7aa917d4552bbea575635bb44a2a
ha272won <103228431+ha272won@users.noreply.github.com>
2022-05-02 17:40:57 -0700

    edit

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -6,10 +6,41 @@ import java.nio.file.Path;
 import java.nio.file.Files;
 
 public class MarkdownParseTest {
+
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
-      
     }
-  
+
+    @Test
+    public void markdownTestFile1() throws IOException{
+        MarkdownParse parse = new MarkdownParse();
+        ArrayList<String> links;
+        
+    links = parse.getLinks(Files.readString(Path.of("maketestfile1.md")));
+        assertEquals(links.size(), 1);
+        assertEquals(links.get(0), "https://something.com");
+    }
+
+    @Test
+    public void markdownTestFile2() throws IOException{
+        MarkdownParse parse = new MarkdownParse();
+        ArrayList<String> links;
+        
+    links = parse.getLinks(Files.readString(Path.of("maketestfile2.md")));
+        assertEquals(links.size(), 2);
+        assertEquals(links.get(0), "https://something.com");
+        assertEquals(links.get(1), "some-thing.html");
+    }
+
+    @Test
+    public void markdownTestFile3() throws IOException{
+        MarkdownParse parse = new MarkdownParse();
+        ArrayList<String> links;
+        
+        links = parse.getLinks(Files.readString(Path.of("maketestfile3.md")));
+        assertEquals(links.size(), 1);
+        assertEquals(links.get(0), "some-thing.html");
+    }
+
 }
--- maketestfile1.md
+++ maketestfile1.md
@@ -1,1 +1,1 @@
-[link1(https://something.com) link2](https://github.com/Evan1Lam/markdown-parser/blob/main/some-page.html)
+[link1](https://something.com) link2](https://github.com/Evan1Lam/markdown-parser/blob/main/some-page.html)
