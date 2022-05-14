1df2f1b0541ea0b93dafbb3184b11fb7d387bf14
ha272won <103228431+ha272won@users.noreply.github.com>
2022-05-14 16:32:38 -0700

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -12,35 +12,11 @@ public class MarkdownParseTest {
         assertEquals(2, 1 + 1);
     }
 
-public class MarkdownParseTest {
-    @Test
-    public void addition() { 
-        assertEquals(2, 1 + 1);
-    }
-
-    @Test
-    public void testDefaultFile() throws IOException{
-        Path fileName = Path.of("test-file.md");
-        String content = Files.readString(fileName);
-        ArrayList<String> links = MarkdownParse.getLinks(content);
-
-        ArrayList<String> expectedOutput = new ArrayList<>();
-        expectedOutput.add("https://something.com");
-        expectedOutput.add("some-thing.html");
-
-        assertEquals(expectedOutput, links);
-    }
-
     @Test
     public void test1() throws IOException{
         Path fileName = Path.of("maketestfile1.md");
         String content = Files.readString(fileName);
         ArrayList<String> links = MarkdownParse.getLinks(content);
-
-        ArrayList<String> expectedOutput = new ArrayList<>();
-        expectedOutput.add("www.google.com");
-        expectedOutput.add("www.youtube.com");
-        expectedOutput.add("reddit.com");
 
         assertEquals(expectedOutput, links);
     }
@@ -51,9 +27,6 @@ public class MarkdownParseTest {
         String content = Files.readString(fileName);
         ArrayList<String> links = MarkdownParse.getLinks(content);
 
-        ArrayList<String> expectedOutput = new ArrayList<>();
-        expectedOutput.add("No links found");
-
         assertEquals(expectedOutput, links);
     }
 
@@ -63,8 +36,6 @@ public class MarkdownParseTest {
         String content = Files.readString(fileName);
         ArrayList<String> links = MarkdownParse.getLinks(content);
 
-        ArrayList<String> expectedOutput = new ArrayList<>();
-        expectedOutput.add("No links found");
 
         assertEquals(expectedOutput, links);
     }
