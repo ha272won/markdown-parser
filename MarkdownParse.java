770af43c93694d2d16452e180ead538610cb59d7
ha272won <103228431+ha272won@users.noreply.github.com>
2022-05-15 00:55:21 -0700

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -7,35 +7,23 @@ import java.util.ArrayList;
 
 
 public class MarkdownParse {
+	public static ArrayList<String> getLinks(String markdown) {
+		ArrayList<String> ans = new ArrayList<>();
+		int currentIndex = 0;
+           
+        	while(currentIndex < markdown.length()) {
+			int openBracket = markdown.indexOf("[", currentIndex);
+			int openParen = markdown.indexOf("(", closeBracket);
+			int closeBracket = markdown.indexOf("]", openBracket);
+			int closeParen = markdown.indexOf(")", openParen);
+			
+			
+	}
 
-    public static ArrayList<String> getLinks(String markdown) {
-        if(!(markdown.contains("(")) || !(markdown.contains(")"))){
-            ArrayList<String> ans = new ArrayList<>();
-            ans.add("No links found");
-
-            return ans;
-        }
-        
-        ArrayList<String> toReturn = new ArrayList<>();
-        int currentIndex = 0;
-        while(currentIndex < markdown.length()) {
-            int openBracket = markdown.indexOf("[", currentIndex);
-            int closeBracket = markdown.indexOf("]", openBracket);
-
-            int openParen = markdown.indexOf("(", closeBracket);
-            int closeParen = markdown.indexOf(")", openParen);
-            //System.out.println(markdown.substring(openParen + 1, closeParen));
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
-            currentIndex = closeParen + 1;
-        }
-        return toReturn;
-    }
-
-
-    public static void main(String[] args) throws IOException {
-        Path fileName = Path.of(args[0]);
-        String content = Files.readString(fileName);
-        ArrayList<String> links = getLinks(content);
-	    System.out.println(links);
-    }
+	public static void main(String[] args) throws IOException {
+        	Path fileName = Path.of(args[0]);
+        	String content = Files.readString(fileName);
+        	ArrayList<String> links = getLinks(content);
+	    	System.out.println(links);
+    	}
 }
