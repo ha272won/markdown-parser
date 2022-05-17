d594d05616f99192a8b3342fcfcd63669f535b7e
ha272won <103228431+ha272won@users.noreply.github.com>
2022-05-16 18:47:20 -0700

    update

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -13,33 +13,42 @@ public class MarkdownParse {
            
         	while(currentIndex < markdown.length()) {
 			int openBracket = markdown.indexOf("[", currentIndex);
-			int openParen = markdown.indexOf("(", closeBracket);
 			int closeBracket = markdown.indexOf("]", openBracket);
+			int openParen = markdown.indexOf("(", closeBracket);
 			int closeParen = markdown.indexOf(")", openParen);
 			
-			if (openBracket == -1) {
-				break;
-			}
-			
-			String closeBracket = markdown.substring(closeBracket+1, closeBracket+2); 
-			
-			if (!closeBracket.equals("(")) {
-				break;
-			}
-			
-			if ( (openBracket ==0) || markdown.charAt(openBracket-1) != '!') {
-				toReturn.add(markdown.substring(openParen + 1, closeParen));
-            			currentIndex = closeParen + 1;
-			}
-			
-		}
-        	return toReturn;	
+
+            if(openBracket == -1 || closeBracket == -1 || openParen == -1 || 
+               closeParen == -1) {
+                break;
+            }
+
+            if(markdown.charAt(0) == '!'){
+                currentIndex = closeParen + 1;
+                continue;
+            }
+
+            if(openBracket != 0 && markdown.charAt(openBracket-1) == '!'){
+                currentIndex = closeParen + 1;
+                continue;
+            }
+
+            if(closeBracket != openParen - 1) {
+                currentIndex = closeParen + 1;
+                continue;
+            }
+
+            toReturn.add(markdown.substring(openParen + 1, closeParen));
+            currentIndex = closeParen + 1;
+        }
+
+		return toReturn;
 	}
 
 	public static void main(String[] args) throws IOException {
         	Path fileName = Path.of(args[0]);
         	String content = Files.readString(fileName);
         	ArrayList<String> links = getLinks(content);
-	    	System.out.println(links);
+	    		System.out.println(links);
     	}
 }
